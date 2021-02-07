87e6b2b5fbd78d739325503e5b625a9e33b120e0
VinuelMancent <61296588+VinuelMancent@users.noreply.github.com>
2021-02-07 19:05:38 +0100

    minor name changes

--- gRPC Client/Src/Client.cs
+++ gRPC Client/Src/Client.cs
@@ -15,25 +15,37 @@ namespace gRPC_Client
 {
     public class Client
     {
-        private GrpcChannelOptions myOptions;
-        private GrpcChannel channel;
-        private Cloud.Cloud.CloudClient client;
-        private string name = "";
+        private GrpcChannelOptions _myOptions;
+        private GrpcChannel _channel;
+        private readonly  Cloud.Cloud.CloudClient _client;
+        private string _name = "";
         
         public Client()
         {
-            this.myOptions = new GrpcChannelOptions
+            this._myOptions = new GrpcChannelOptions
             {
                 Credentials = ChannelCredentials.Insecure,
             };
-            this.channel = GrpcChannel.ForAddress("http://localhost:9000", myOptions);
-            this.client = new Cloud.Cloud.CloudClient(channel);
+            this._channel = GrpcChannel.ForAddress("http://localhost:9000", _myOptions);
+            this._client = new Cloud.Cloud.CloudClient(_channel);
+        }
+
+        public Client(string name)
+        {
+            this._myOptions = new GrpcChannelOptions
+            {
+                Credentials = ChannelCredentials.Insecure,
+            };
+            this._channel = GrpcChannel.ForAddress("http://localhost:9000", _myOptions);
+            this._client = new Cloud.Cloud.CloudClient(_channel);
+            this._name = name;
+
         }
         //Greeter sends a simple message to the server and prints out its response
         public void Greeter()
         {
             //Send a simple request to the server and save the response in reply
-            var reply = client.SayHello(new Message
+            var reply = _client.SayHello(new Message
             {
                 Body = "Greetings to the server",
             });
@@ -44,7 +56,7 @@ namespace gRPC_Client
         //ToDo add a error message in proto when the file cant be found
         public void Download(string filename)
         {
-            var reply = client.Download(new Message
+            var reply = _client.Download(new Message
             {
                 Body = filename,
             });
@@ -56,7 +68,7 @@ namespace gRPC_Client
         public void Upload(string filename)
         {
             var file = FileSystem.ReadAllBytes(filename);
-            var reply = client.Upload(new Cloud.File
+            var reply = _client.Upload(new Cloud.File
             {
                 File_ = ByteString.CopyFrom(file),
                 Filename = filename,
@@ -66,7 +78,7 @@ namespace gRPC_Client
 
         public string[] GetFiles()
         {
-            var reply = client.GetFiles(new Message
+            var reply = _client.GetFiles(new Message
             {
                 Body = ".",
             });
