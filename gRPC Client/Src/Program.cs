ae86c77c9a0bc4cb093f80293059b91ec7735326
VinuelMancent <Beetel6411>
2021-02-21 18:48:55 +0100

    implemented my own logger

--- gRPC Client/Src/Program.cs
+++ gRPC Client/Src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Logger;
+using Logger.Src;
 
 namespace gRPC_Client
 {
@@ -6,7 +8,9 @@ namespace gRPC_Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting client!");
+            var log = Manager.Instance;
+            log.Info("default", "Starting client!");
+            //Console.WriteLine("Starting client!");
             var myClient = new Client();
             myClient.Greeter();
             //myClient.Download("download.txt");
